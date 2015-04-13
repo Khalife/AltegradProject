3d98e64b148c762b0426dd25a62d9b9733e5a9f9
Ethiy <oussama.ennafii@polytechnique.org>
2015-04-14 00:10:45 +0200

    new version

--- code/BagOfWords/pca.py
+++ code/BagOfWords/pca.py
@@ -1,6 +1,9 @@
 
 
-def pca(matrix):
+def pca(matrix,dimension):
+	""" performs PCA on matrix
+		dimension is the dimension of the projection
+		returns a projection into a lower dimension embedding"""
 	Mean=mean(matrix,axis=0);
 	M=reshape(Mean,data.shape[0],1);
 	C=matrix-M;
@@ -9,3 +12,4 @@ def pca(matrix):
 
 	perm=argsort(S)[::-1];
 	U=U[:,perm];
+	return dot(C,U[:,:dimension]);
