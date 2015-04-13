56f052dd943bca46ddfe150711153536834cd0be
Khalife <sam_411@hotmail.fr>
2015-04-13 23:42:36 +0200

    main

--- main.py
+++ main.py
@@ -32,19 +32,24 @@ X_ = vectorizer.fit_transform(contents) # under Hashed form
 X=X_.toarray()	# under array form : Document Word Matrix
 
 # Perform dimensionality reduction over X (SVD)
-#print shape(X)
+# print shape(X)
 disp('Preprocessing...')
 
 transformer = TfidfTransformer()
 Tfidf = transformer.fit_transform(X)
 T=Tfidf.toarray() # TF IDF matrix
 
+#print shape(T)
+#print shape(tile(mean(T,1),(shape(T)[1],1)).transpose())
+T = T - tile(mean(T,1),(shape(T)[1],1)).transpose() # center matrix
+
 U, s, V = linalg.svd(T)
 U200=U[:,0:200]
-V200=V[0:200,:]
-S200=diag(s)[:200,:200]
-X200=dot(dot(U200,S200),V200)
-X=X200
+##V200=V[0:200,:]
+##S200=diag(s)[:200,:200]
+##X200=dot(dot(U200,S200),V200)
+#
+X=dot(T,U)
 
 # # samples in column, features in lines
 Xtrain = X
@@ -56,22 +61,22 @@ savetxt('Xtrain.txt',Xtrain)
 savetxt('Ytrain.txt',Ytrain)
 
 ## Read from text
-lines = [line.strip() for line in open('Xtrain.txt')]
-Xtrain = []
-for line in lines:
-    Xtrain.append([float(x) for x in line.split()])
-
-lines = [line.strip() for line in open('Ytrain.txt')]
-L=shape(lines)
-Ytrain=ones(L[0])
-i=0
-for line in lines:
-    Ytrain[i]=line
-    Ytrain[i]=int(Ytrain[i])
-    i=i+1
-
-print shape(Ytrain)
-print shape(Xtrain)
+#lines = [line.strip() for line in open('Xtrain.txt')]
+#Xtrain = []
+#for line in lines:
+#    Xtrain.append([float(x) for x in line.split()])
+#
+#lines = [line.strip() for line in open('Ytrain.txt')]
+#L=shape(lines)
+#Ytrain=ones(L[0])
+#i=0
+#for line in lines:
+#    Ytrain[i]=line
+#    Ytrain[i]=int(Ytrain[i])
+#    i=i+1
+#
+#print shape(Ytrain)
+#print shape(Xtrain)
 #permutation random des indices
     
 #s=list(range(L[0]))
@@ -120,64 +125,66 @@ disp('Cross Validation...')
 # Evaluation : Test on real test set
 
 ## Read and preprocess test data ##############################
-disp('Preprocessing on test set')
-lines = [line.strip() for line in open('r8_test_stemmed.txt')]
-labels = []
-contents = []
-for line in lines:
-    [label1,content1]=line.split(' ',1)
-    labels.append(label1)
-    contents.append(content1)
-N = len(lines)
-
-print shape(contents)
-print shape(labels)
-vectorizer = CountVectorizer(min_df=1)
-X_ = vectorizer.fit_transform(contents) # under Hashed form
-X=X_.toarray()	# under array form  : Document Word Matrix
-
-transformer = TfidfTransformer()
-Tfidf = transformer.fit_transform(X)
-T=Tfidf.toarray() # TF IDF matrix
-
-U, s, V = linalg.svd(T)
-U200=U[:,0:200]
-V200=V[0:200,:]
-S200=diag(s)[:200,:200]
-X200=dot(dot(U200,S200),V200)
-X=X200
-
-
- # samples in column, features in lines
-Xtest = X
-le = preprocessing.LabelEncoder()
-le.fit(labels)
-Ytest=le.transform(labels)
-
-savetxt('Xtest.txt',Xtest)
-savetxt('Ytest.txt',Ytest) 
-
-## Read from text 
-lines = [line.strip() for line in open('Xtest.txt')]
-Xtest = []
-for line in lines:
-    Xtest.append([float(x) for x in line.split()])
-
-lines = [line.strip() for line in open('Ytest.txt')]
-L=shape(lines)
-Ytest=ones(L[0])
-i=0
-for line in lines:
-    Ytest[i]=line
-    Ytest[i]=int(Ytest[i])
-    i=i+1
-
-print shape(Ytest)
-print shape(Xtest)
-# ###################################################
+#disp('Preprocessing on test set')
+#lines = [line.strip() for line in open('r8_test_stemmed.txt')]
+#labels = []
+#contents = []
+#for line in lines:
+#    [label1,content1]=line.split(' ',1)
+#    labels.append(label1)
+#    contents.append(content1)
+#N = len(lines)
 #
-#error_ = clf.score(Xtest,Ytest)
-#print error_
+#print shape(contents)
+#print shape(labels)
+#vectorizer = CountVectorizer(min_df=1)
+#X_ = vectorizer.fit_transform(contents) # under Hashed form
+#X=X_.toarray()	# under array form  : Document Word Matrix
 #
+#transformer = TfidfTransformer()
+#Tfidf = transformer.fit_transform(X)
+#T=Tfidf.toarray() # TF IDF matrix
+#T=T-tile(mean(T,1), (1, shape(T)[2]))
 #
+#U, s, V = linalg.svd(T)
+#U200=U[:,0:200]
+#V200=V[0:200,:]
+##S200=diag(s)[:200,:200]
+##X200=dot(dot(U200,S200),V200)
+##X=X200
 #
+#X = dot(T,U200)
+
+# # samples in column, features in lines
+#Xtest = X
+#le = preprocessing.LabelEncoder()
+#le.fit(labels)
+#Ytest=le.transform(labels)
+#
+#savetxt('Xtest.txt',Xtest)
+#savetxt('Ytest.txt',Ytest) 
+#
+### Read from text 
+#lines = [line.strip() for line in open('Xtest.txt')]
+#Xtest = []
+#for line in lines:
+#    Xtest.append([float(x) for x in line.split()])
+#
+#lines = [line.strip() for line in open('Ytest.txt')]
+#L=shape(lines)
+#Ytest=ones(L[0])
+#i=0
+#for line in lines:
+#    Ytest[i]=line
+#    Ytest[i]=int(Ytest[i])
+#    i=i+1
+#
+#print shape(Ytest)
+#print shape(Xtest)
+## ###################################################
+##
+##error_ = clf.score(Xtest,Ytest)
+##print error_
+##
+##
+##
