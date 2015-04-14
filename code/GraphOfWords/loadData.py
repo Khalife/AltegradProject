19de91527b8867779b364881be13152daab47ca8
Ethiy <oussama.ennafii@polytechnique.org>
2015-04-14 22:03:01 +0200

    graph of words initiation

---
+++ code/GraphOfWords/loadData.py
@@ -0,0 +1,22 @@
+def loadData(path,trainData):
+
+	""" def loadData(path,kindOfData):
+			path: is the text path representing the data.
+			trainData: specifies the kind of data. If it is "True" then it is a train data.
+
+		returns a collection of documents(lists) and there labels.
+	"""
+
+	with open(path ,"r") as File:
+		labels = []
+		documents = []
+		for line in File:
+			content = line.split('\t')
+			if trainData:
+				labels.append(content[0])
+				document = content[1:]
+				documents.append(document)
+			else:
+				documents.append(content)
+	
+	return {"labels" : labels , "documents" : documents}
