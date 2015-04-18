8979335ac0bb1a8daa6b2ae4fd886cff8823ce35
Ethiy <oussama.ennafii@polytechnique.org>
2015-04-18 13:46:21 +0200

    document term matrix

---
+++ code/GraphOfWords/documentTermMatrix.py
@@ -0,0 +1,34 @@
+from documentWordMatrix import documentWordMatrix
+
+import numpy as np 
+
+import numpy.matlib as mtlb
+
+def documentTermMatrix(documents , window , directed , weighted, parameter):
+	""" def documentTermMatrix(documents , window , directed , weighted):
+			documents: is the set of documents loaded.
+			window: is the number of words considered at a time 
+			directed: is a boolean that determines if the graph is directed
+			weighted: is a boolean determines if the graph is weighted
+
+			returns: 
+	"""
+
+	(DWM , presenceMatrix , avdl, lengths , words) = documentWordMatrix(documents ,window , directed , weighted)
+	(numberOfDocuments , numberOfWords) = DWM.shape()
+	termDocumentOccurence = np.sum(presenceMatrix , axis = 0)
+	auxilary = numberOfDocuments * np.ones( numberOfWords )
+	IDF_ = np.divide(auxilary , termDocumentOccurence)
+	IDF = mtlib.repmat(IDF_ , numberOfDocuments)
+
+	pivotMatrix = np.array((numberOfDocuments , numberOfWords))
+	for length in lengths:
+		documentNumber = length[0]
+		documentLength = length[1]
+		pivotMatrix[ documentNumber - 1 ] = 1/(1 - parameter + parameter * documentLength / avdl) * np.ones(numberOfWords)
+
+	TWM = np.multiply(DWM , pivotMatrix)
+
+	TW_IDF = np.multiply(TWM , IDF)
+
+	return (TWM , IDF , TW_IDF)
