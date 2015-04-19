47d711dcf6cd08698321f3b69c7e17fd6fe723da
Ethiy <oussama.ennafii@polytechnique.org>
2015-04-19 22:25:01 +0200

    bag of words bis

---
+++ code/BagOfWordsbis/getTFIDF.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from numpy import tile
+from numpy import mean
+from numpy import shape
+
+def getTFIDF(contents):
+	vectorizer = CountVectorizer(min_df=1)
+	X_ = vectorizer.fit_transform(contents) # under Hashed form
+	X=X_.toarray()	# under array form  : Document Word Matrix	
+	transformer = TfidfTransformer()
+	Tfidf = transformer.fit_transform(X)
+	T=Tfidf.toarray() # centered TF IDF matrix
+	return T- tile(mean(T,1),(shape(T)[1],1)).transpose() 
