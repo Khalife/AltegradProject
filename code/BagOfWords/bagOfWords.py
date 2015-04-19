38c1f03327e8a572df3b459d5af76f35f1e85d2c
Khalife <sam_411@hotmail.fr>
2015-04-19 13:45:55 -0300

    bag

--- code/BagOfWords/bagOfWords.py
+++ code/BagOfWords/bagOfWords.py
@@ -1,205 +1,88 @@
-from loadData import loadData
-import random as rnd
+#bagOfWords method
 from numpy import *
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib import rc
 import math
-from sklearn.feature_extraction.text import TfidfTransformer
-#from tfidf import tfidf
-#from gensim import corpora, models, similarities
-from sklearn.feature_extraction.text import CountVectorizer
 from time import time
 from sklearn import cross_validation
 from sklearn import datasets
 from sklearn import svm
-from sklearn import preprocessing
+from toTFIDF import toTFIDF
+from PCA import PCA
+from transformLabels import transformLabels
+from sklearn.decomposition import TruncatedSVD
+from sklearn.random_projection import sparse_random_matrix
 
 
+#########################################################
+# 1- 
+disp('Preprocessing')
+with open('../data/r8_train_stemmed.txt') as lines:
+#with open('r8_train_stemmed.txt') as lines:
+    labels = []
+    contents = []
+    for line in lines:
+        [label, content] = line.split('\t')
+        labels.append(label)
+        contents.append(content)
 
-# Load the train data
-
-trainData = True 
-path = "../data/r8_train_stemmed.txt"
-#data = loadData(path,trainData)
-with open(path,'r') as File:
-	lines = [line.strip() for line in File]
-	labels = []
-	contents = []
-	for line in lines:
-		[label,content]=line.split('\t')
-    	labels.append(label)
-    	contents.append(content)
-print contents
-#labels = data["labels"]
-#contents = data["documents"]
-#print shape(contents)
-vectorizer = CountVectorizer(min_df=1)
-X_ = vectorizer.fit_transform(contents) # under Hashed form
-X=X_.toarray()	# under array form : Document Word Matrix
-
-
-# Perform dimensionality reduction over X (SVD)
-
-# print shape(X)
-disp('Preprocessing...')
-
-#print shape(X)
-print "Preprocessing..."
-
-transformer = TfidfTransformer()
-Tfidf = transformer.fit_transform(X)
-T=Tfidf.toarray() # TF IDF matrix
-
-#print shape(T)
-#print shape(tile(mean(T,1),(shape(T)[1],1)).transpose())
-T = T - tile(mean(T,1),(shape(T)[1],1)).transpose() # center matrix
-
-U, s, V = linalg.svd(T)
-U200=U[:,0:200]
-##V200=V[0:200,:]
-##S200=diag(s)[:200,:200]
-##X200=dot(dot(U200,S200),V200)
-#
-print shape(X_)
+T=toTFIDF(contents)
 print shape(T)
-print shape(U)
-X=dot(T,U200)
+svd = TruncatedSVD(n_components=100, random_state=42)
+print 'PCA...'
+svd.fit(T) 
+X_train = svd.transform(T)
+Y_train = transformLabels(labels)
 
-# # samples in column, features in lines
-Xtrain = X
-le = preprocessing.LabelEncoder()
-le.fit(labels)
-Ytrain=le.transform(labels) 
-#
-savetxt('Xtrain.txt',Xtrain)
-savetxt('Ytrain.txt',Ytrain)
+########################################################
+# 2. Learning and Cross validation 
+# disp('Cross Validation...')
+n_test=5
+n_cross=5
+c=linspace(0.1,1,n_cross)
+Gamma=linspace(0.1,1,n_cross)
+error=zeros(n_cross*n_cross)
 
-## Read from text
-#lines = [line.strip() for line in open('Xtrain.txt')]
-#Xtrain = []
-#for line in lines:
-#    Xtrain.append([float(x) for x in line.split()])
-#
-#lines = [line.strip() for line in open('Ytrain.txt')]
-#L=shape(lines)
-#Ytrain=ones(L[0])
-#i=0
-#for line in lines:
-#    Ytrain[i]=line
-#    Ytrain[i]=int(Ytrain[i])
-#    i=i+1
-#
-#print shape(Ytrain)
-#print shape(Xtrain)
-#permutation random des indices
-    
-#s=list(range(L[0]))
-#p=random.shuffle(s)
-#for i in range(L[0])    
-#    Ytrain[i]=p[i]
+for i in range(n_cross):
+    print i
+    for j in range(n_cross):
+        for k in range(n_test):
+            X_train1, X_test1, y_train1, y_test1 = cross_validation.train_test_split(X_train, Y_train, test_size=(k+1)/float(n_test+1), random_state=0)
+            clf = svm.SVC(kernel='rbf', C=c[i],gamma=Gamma[j]).fit(X_train1, y_train1)
+            error_ = clf.score(X_test1,y_test1)            
+            error[i+n_cross*j]= error[i+n_cross*j] + error_
+    	error[i+n_cross*j]=error[i+n_cross*j] / n_test # take mean 
 
+index_min=argmin(-error)
+index1 = index_min % n_cross
+C_=c[index1]
+gamma_ = Gamma[( index_min - index1)/n_cross]
 
+clf = svm.SVC(kernel='rbf', C=C_, gamma = gamma_).fit(X_train, Y_train)
+#clf = svm.SVC(kernel='rbf',C=1).fit(X_train,Y_train)
 
+########################################################
+# 3.  
+disp('Preprocessing on test set')
+with open('../data/r8_test_stemmed.txt') as lines:
+    labels = []
+    contents = []
+    for line in lines:
+        [label,content]=line.split(' ',1)
+        labels.append(label)
+        contents.append(content)
 
-#j = subsample_indices(x, 2)
+T=toTFIDF(contents)
+svd = TruncatedSVD(n_components=100, random_state=42)
+print 'PCA test...'
+svd.fit(T) 
+X_test = svd.transform(T)
+Y_test = transformLabels(labels)
 
-#print [x[t] for t in j[-1]]
-#print [x[t] for t in j[1]]
-
- # 2. Learning and Cross validation 
-disp('Cross Validation...')
-#n_test=10
-#n_cross=10
-#c=linspace(1,n_cross,n_cross)
-#Gamma=linspace(1,n_cross,n_cross)
-#error=zeros(n_cross*n_cross)
-# iris = datasets.load_iris()
-# iris.data.shape, iris.target.shape
-#erreur = inf
-#for i in range(n_cross):
-#    for j in range(n_cross):
-#        for k in range(n_test):
-#         print k   
-#         X_train, X_test, y_train, y_test = cross_validation.train_test_split(Xtrain, Ytrain, test_size=(k+1)/float(n_test+1), random_state=0)
-#         clf = svm.SVC(kernel='linear', C=c[i],gamma=Gamma[j]).fit(X_train, y_train)
-#         error_ = clf.score(X_test,y_test)            
-#         if error_ < erreur:
-#          erreur = error_
-#          error[i+n_cross*j]=error_
-#
-#index_min=argmin(error)
-#C=c[index_min % n_cross]
-#gamma = Gamma[( index_min - C)/n_cross]
-
-# # 3. Retrain on full train set
-#X_train, X_test, y_train, y_test = cross_validation.train_test_split(Xtrain, Ytrain, test_size=(i+1)/float(n_test), random_state=0)
-#clf = svm.SVC(kernel='linear', C=1).fit(Xtrain, Ytrain)
+error_ = clf.score(X_test,Y_test)
+print error_
 
 
-# Evaluation : Test on real test set
 
-## Read and preprocess test data ##############################
-#disp('Preprocessing on test set')
-#lines = [line.strip() for line in open('r8_test_stemmed.txt')]
-#labels = []
-#contents = []
-#for line in lines:
-#    [label1,content1]=line.split(' ',1)
-#    labels.append(label1)
-#    contents.append(content1)
-#N = len(lines)
-#
-#print shape(contents)
-#print shape(labels)
-#vectorizer = CountVectorizer(min_df=1)
-#X_ = vectorizer.fit_transform(contents) # under Hashed form
-#X=X_.toarray()	# under array form  : Document Word Matrix
-#
-#transformer = TfidfTransformer()
-#Tfidf = transformer.fit_transform(X)
-#T=Tfidf.toarray() # TF IDF matrix
-#T=T-tile(mean(T,1), (1, shape(T)[2]))
-#
-#U, s, V = linalg.svd(T)
-#U200=U[:,0:200]
-#V200=V[0:200,:]
-##S200=diag(s)[:200,:200]
-##X200=dot(dot(U200,S200),V200)
-##X=X200
-#
-#X = dot(T,U200)
 
-# # samples in column, features in lines
-#Xtest = X
-#le = preprocessing.LabelEncoder()
-#le.fit(labels)
-#Ytest=le.transform(labels)
-#
-#savetxt('Xtest.txt',Xtest)
-#savetxt('Ytest.txt',Ytest) 
-#
-### Read from text 
-#lines = [line.strip() for line in open('Xtest.txt')]
-#Xtest = []
-#for line in lines:
-#    Xtest.append([float(x) for x in line.split()])
-#
-#lines = [line.strip() for line in open('Ytest.txt')]
-#L=shape(lines)
-#Ytest=ones(L[0])
-#i=0
-#for line in lines:
-#    Ytest[i]=line
-#    Ytest[i]=int(Ytest[i])
-#    i=i+1
-#
-#print shape(Ytest)
-#print shape(Xtest)
-## ###################################################
-##
-##error_ = clf.score(Xtest,Ytest)
-##print error_
-##
-##
-##
